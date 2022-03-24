69d7ad5b46bd39e9498a59637e3c916ab593f891
ftrahman@bu.edu <ftrahman@bu.edu>
2022-03-24 15:10:49 -0400

    adding targetsum and removeDupes

--- study-guide/grokking/TwoPointers.java
+++ study-guide/grokking/TwoPointers.java
@@ -1,4 +1,36 @@
 public class TwoPointers {
+    public static int[] targetSum(int[] arr, int targetSum) {
+        // Time: O(n), Space: O(1)
+        int p1 = 0;
+        int p2 = arr.length - 1;
+        while (p1 < p2) {
+            if (arr[p1] + arr[p2] == targetSum)
+                break;
+            else if (arr[p1] + arr[p2] > targetSum)
+                p2--;
+            else
+                p1++;
+        }
+        return new int[] { p1, p2 };
+    }
+
+    public static int removeDuplicates(int[] arr) {
+        // Time: O(n), Space: O(1)
+        int count = 1;
+        int p1 = 0;
+        int p2 = 1;
+        while (p2 < arr.length) {
+            if (arr[p1] == arr[p2]) {
+                p2++;
+            } else {
+                p1 = p2;
+                p2++;
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static int[] makeSquares(int[] arr) {
         // Time: O(n), Space: O(n)
         // iterate array only one; take up space for new array
