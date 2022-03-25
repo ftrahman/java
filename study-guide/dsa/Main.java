e787ee2a0486949c61d837dcf16a0f4d304f6993
ftrahman@bu.edu <ftrahman@bu.edu>
2022-03-25 13:36:00 -0400

    added tester functions for DLL

--- study-guide/dsa/Main.java
+++ study-guide/dsa/Main.java
@@ -1,18 +1,51 @@
 public class Main {
     public static void main(String[] args) {
-        LinkedList LL = new LinkedList();
-        LL.addToHead(2);
-        LL.addToHead(8);
-        LL.addToHead(6);
-        LL.printList();
-        LL.addToTail(4);
-        LL.addToTail(7);
-        LL.printList();
-        LL.addToHead(2);
-        LL.printList();
-        LL.removeHead();
-        LL.removeHead();
-        LL.removeHead();
-        LL.printList();
+        // Test code for LL Class
+
+        // LinkedList LL = new LinkedList();
+        // LL.printList();
+        // LL.removeHead();
+        // LL.addToHead(2);
+        // LL.addToHead(8);
+        // LL.addToHead(6);
+        // LL.printList();
+        // LL.addToTail(4);
+        // LL.addToTail(7);
+        // LL.printList();
+        // LL.swap(8, 7);
+        // LL.printList();
+        // LL.addToHead(2);
+        // LL.printList();
+        // LL.removeHead();
+        // LL.removeHead();
+        // LL.removeHead();
+        // LL.printList();
+
+        // Test code for CLL Class
+
+        // DoublyLinkedList DLL = new DoublyLinkedList();
+        // DLL.addToHead(3);
+        // DLL.addToHead(2);
+        // DLL.addToHead(6);
+        // DLL.addToHead(8);
+        // DLL.printList();
+        // DLL.removeTail();
+        // DLL.addToTail(5);
+        // DLL.addToTail(9);
+        // DLL.printList();
+        // DLL.removeTail();
+        // DLL.printList();
+        // DLL.addToTail(1);
+        // DLL.printList();
+        // DLL.addToTail(9);
+        // DLL.printList();
+        // DLL.addToHead(7);
+        // DLL.printList();
+        // DLL.removeByData(1);
+        // DLL.printList();
+        // DLL.removeHead();
+        // DLL.removeHead();
+        // DLL.printList();
+
     }
 }
