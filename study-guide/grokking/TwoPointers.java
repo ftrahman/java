a77b69415e4022d271bb1f3c96a77809d3c96b2d
ftrahman@bu.edu <ftrahman@bu.edu>
2022-03-24 22:00:42 -0400

    finished searchTripletsForNearest and added comments to explain

--- study-guide/grokking/TwoPointers.java
+++ study-guide/grokking/TwoPointers.java
@@ -45,7 +45,8 @@ public class TwoPointers {
         int p2 = arr.length - 1;
         int curIndex = arr.length - 1;
         while (p1 <= p2) {
-            if (Math.pow(arr[p1], 2) > Math.pow(arr[p2], 2)) {
+            if (Math.pow(arr[p1], 2) > Math.pow(arr[p2], 2)) { // searching for biggest square at beginning and end of
+                                                               // array, since we are working with negative numbers
                 squares[curIndex--] = (int) Math.pow(arr[p1], 2); // need to cast because Math.pow will return a double
                 p1++;
             } else {
@@ -58,34 +59,67 @@ public class TwoPointers {
 
     public static List<List<Integer>> searchTriplets(int[] arr) {
         // Time: O(n^2), Space: O(n)
+        // here we are trying to find unique triplets that equal 0
+        // can approach using X + Y + Z = 0 which can then be approached
+        // as X + Y = -Z
         List<List<Integer>> triplets = new ArrayList<>();
         Arrays.sort(arr); // unfortunately, need to sort the array
-        for (int curIndex = 0; curIndex < arr.length - 2; curIndex++) {
-            if (curIndex > 0 && arr[curIndex] == arr[curIndex + 1]) {
+        for (int curIndex = 0; curIndex < arr.length - 2; curIndex++) { // curIndex acts as pointer for Z
+            if (curIndex > 0 && arr[curIndex] == arr[curIndex + 1]) { // we want to avoid triplets for the same values
                 continue;
             }
-            int p1 = curIndex + 1;
-            int p2 = arr.length - 1;
-            int curTarget = -arr[curIndex];
-            while (p1 < p2) {
-                if (arr[p1] + arr[p2] == curTarget) {
+            int p1 = curIndex + 1; // p1 acts as pointer for X
+            int p2 = arr.length - 1; // p2 acts as pointer for Y
+            int curTarget = -arr[curIndex]; // curTarget acts as our -Z value
+            while (p1 < p2) { // while X does not equal Y
+                if (arr[p1] + arr[p2] == curTarget) { // X + Y = -Z, add to triplets
                     List<Integer> trip = Arrays.asList(arr[p1], arr[p2], arr[curIndex]);
                     triplets.add(trip);
-                    p1++;
-                    p2--;
+                    p1++; // increment X
+                    p2--; // decrement Y
                     while (p1 < p2 && arr[p1] == arr[p1 - 1]) {
                         p1++; // here we need to skip same element to avoid dupes
                     }
                     while (p1 < p2 && arr[p2] == arr[p2 + 1]) {
                         p2--; // here we need to skip same element to avoid dupes
                     }
-                } else if (arr[p1] + arr[p2] < curTarget)
+                } else if (arr[p1] + arr[p2] < curTarget) // if X + Y < -Z, increment X
+                    p1++;
+                else // if X + Y > -Z, decrement Y
+                    p2--;
+            }
+        }
+        return triplets;
+    }
+
+    public static int searchTripletForNearest(int[] arr, int targetSum) {
+        // Time: O(n^2), Space: O(n)
+        // here we can approach similarly to the previous problem, except we are now
+        // searching for the the closest possible sum to the target among triplets
+        Arrays.sort(arr);
+        int minDiff = Integer.MAX_VALUE; // need to keep track of the current minimum difference
+        for (int curIndex = 0; curIndex < arr.length - 2; curIndex++) {
+            int p1 = curIndex + 1; // p1 is our X pointer
+            int p2 = arr.length - 1; // p2 is our Y pointer
+            int curVal = arr[curIndex]; // curVal acts as our Z value
+            while (p1 < p2) {
+                int curMin = targetSum - curVal - arr[p1] - arr[p2]; // the currentMin difference from target sum is
+                                                                     // (target - (X + Y + Z))
+                if (curMin == 0) // the minimum difference is 0, we have found the closest minimum difference
+                    return targetSum;
+                if (Math.abs(curMin) < Math.abs(minDiff) // if the abs(currentMin) is smaller than the abs(savedMin) OR if
+                                                         // the abs(currentMin) is the same as the abs(savedMin) AND the
+                                                         // currentMin is a greater value than the savedMin
+                        || Math.abs(curMin) == Math.abs(minDiff) && curMin > minDiff) {
+                    minDiff = curMin; // then change the savedMin to the currentMin
+                }
+                if (curMin > 0)
                     p1++;
                 else
                     p2--;
             }
         }
-        return triplets;
+        return targetSum - minDiff;
     }
 
     public static void main(String[] args) {
