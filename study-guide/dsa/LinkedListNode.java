f298de4a17fe3c222fe668a61f724280ff5bf56e
ftrahman@bu.edu <ftrahman@bu.edu>
2022-03-25 11:22:38 -0400

    added more functionality for LL for later hashtable use/DoubleLL use

--- study-guide/dsa/LinkedListNode.java
+++ study-guide/dsa/LinkedListNode.java
@@ -1,12 +1,20 @@
 public class LinkedListNode {
-    LinkedListNode prev;
-    LinkedListNode next;
-    int value;
+    private LinkedListNode prev;
+    private LinkedListNode next;
+    private int value;
+    private String key;
 
     public LinkedListNode(int value) {
         this.next = null;
         this.prev = null;
         this.value = value;
+    }
+
+    public LinkedListNode(String key, int value) {
+        this.next = null;
+        this.prev = null;
+        this.value = value;
+        this.key = key;
     }
 
     public void setNext(LinkedListNode node) {
@@ -23,5 +31,13 @@ public class LinkedListNode {
 
     public LinkedListNode getPrev() {
         return this.prev;
+    }
+
+    public int getVal() {
+        return this.value;
+    }
+
+    public String getKey() {
+        return this.key;
     }
 }
