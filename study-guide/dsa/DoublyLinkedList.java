3e721f10b6b10a79b0eb743f7c6e14ac8443e03e
ftrahman@bu.edu <ftrahman@bu.edu>
2022-03-25 13:36:58 -0400

    added DLL with constructor, addH/addT/removeH/removeT/removeData methods

---
+++ study-guide/dsa/DoublyLinkedList.java
@@ -0,0 +1,104 @@
+import java.util.LinkedList;
+
+public class DoublyLinkedList {
+    public LinkedListNode head;
+    public LinkedListNode tail;
+    public int length;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    public void addToHead(int data) {
+        LinkedListNode newNode = new LinkedListNode(data);
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.setPrev(newNode);
+            newNode.setNext(this.head);
+            this.head = newNode;
+        }
+    }
+
+    public void addToTail(int data) {
+        LinkedListNode newNode = new LinkedListNode(data);
+        if (this.tail == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.setNext(newNode);
+            newNode.setPrev(this.tail);
+            this.tail = newNode;
+        }
+    }
+
+    public int removeHead() {
+        if (this.head == null) {
+            return -1;
+        }
+        LinkedListNode remove = this.head;
+        this.head = this.head.getNext();
+        if (this.head != null)
+            this.head.setPrev(null);
+        if (remove == this.tail)
+            removeTail();
+        return remove.getVal();
+    }
+
+    public int removeTail() {
+        if (this.tail == null) {
+            return -1;
+        }
+        LinkedListNode remove = this.tail;
+        this.tail = this.tail.getPrev();
+        if (this.tail != null)
+            this.tail.setNext(null);
+        if (remove == this.head) {
+            removeHead();
+        }
+        return remove.getVal();
+    }
+
+    public int removeByData(int data) {
+        LinkedListNode cur = this.head;
+        if (cur == null)
+            return -1;
+        while (cur != null) {
+            if (cur.getVal() == data) {
+                break;
+            }
+            cur = cur.getNext();
+        }
+        if (cur == null)
+            return -1;
+        if (cur == this.head)
+            removeHead();
+        if (cur == this.tail)
+            removeTail();
+        else {
+            LinkedListNode prev = cur.getPrev();
+            LinkedListNode next = cur.getNext();
+            prev.setNext(next);
+            next.setPrev(prev);
+        }
+        System.out.println(cur.getVal());
+        return cur.getVal();
+    }
+
+    public void printList() {
+        if (this.head == null) {
+            System.out.print("(LENGTH " + this.length + "): null <->\n");
+        } else {
+            LinkedListNode cur = this.head;
+            System.out.print("(LENGTH " + this.length + "): ");
+            while (cur.getNext() != null) {
+                System.out.print(cur.getVal() + " <-> ");
+                cur = cur.getNext();
+            }
+            System.out.print(cur.getVal() + " <-> null\n");
+        }
+    }
+}
