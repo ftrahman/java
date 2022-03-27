c66a0c95c8c28b45cb599c14cc93e96da2bf620b
ftrahman@bu.edu <ftrahman@bu.edu>
2022-03-27 16:14:09 -0400

    added bfs and dfs traversals to tree

--- study-guide/dsa/Tree.java
+++ study-guide/dsa/Tree.java
@@ -1,3 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Tree {
-    
+    public TreeNode root;
+
+    public Tree(TreeNode root) {
+        this.root = root;
+    }
+
+    public void dfs(TreeNode cur) {
+        System.out.println(cur.data + " ");
+        for (TreeNode child : cur.children) {
+            dfs(child);
+        }
+    }
+
+    public void bfs() {
+        TreeNode cur = this.root;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(cur);
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            System.out.println(cur.data + " ");
+            queue.addAll(cur.children);
+        }
+    }
+
 }
