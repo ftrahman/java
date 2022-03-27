3113421f8eefa60858ac2af6b1578d14456fd324
ftrahman@bu.edu <ftrahman@bu.edu>
2022-03-27 16:05:10 -0400

    adding all graph/tree structure classes: graph, edge, vertex and tree, treeNode

---
+++ study-guide/dsa/TreeNode.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class TreeNode {
+    private Object data;
+    private ArrayList<TreeNode> children;
+
+    public TreeNode(Object data) {
+        this.children = new ArrayList<TreeNode>();
+        this.data = data;
+    }
+
+    public void addChild(Object data) {
+        this.children.add(new TreeNode(data));
+    }
+
+    public void addChild(TreeNode child) {
+        this.children.add(child);
+    }
+
+    public void removeChild(TreeNode child) {
+        if (this.children.isEmpty())
+            return;
+        else if (this.children.contains(child)) {
+            this.children.remove(child);
+        } else {
+            for (TreeNode cur : this.children) {
+                cur.removeChild(child);
+            }
+        }
+    }
+
+    public void removeChild(Object data) {
+        if (this.children.isEmpty())
+            return;
+        for (TreeNode cur : this.children) {
+            if (cur.data == data) {
+                this.children.remove(cur);
+                return;
+            }
+        }
+        for (TreeNode cur : this.children) {
+            cur.removeChild(data);
+        }
+    }
+}
